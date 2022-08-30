240eb4006d81bf419ecd27bbfc63190dbb8c65b4
Diana-nyamai <dnyamai.dn@gmail.com>
2022-08-30 10:53:22 +0300

    -refactoring the process component code

--- src/components/Process.js
+++ src/components/Process.js
@@ -3,10 +3,54 @@ import '../css/process.css';
 import Aos from 'aos';
 import 'aos/dist/aos.css'
 
+const Span = {
+  fontSize:"42px", 
+  paddingRight: "15px"
+}
+
 function Process() {
     useEffect(() =>{
         Aos.init();
     },[])
+
+    const timeline= [
+      {
+        row: 'row row-1',
+        color: 'c-1',
+        icon: 'fa fa-clipboard-question',
+        title: 'In depth interview',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil deleniti, voluptatibus dolores fugiat temporibus quia, laudantium veritatis in eum dolorum error dolore neque autem ut consectetur mollitia libero nam officia.'
+      },
+      {
+        row: 'row row-2',
+        color: 'c-2',
+        icon: 'fa fa-chalkboard',
+        title: 'date & relationship coaching',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil deleniti, voluptatibus dolores fugiat temporibus quia, laudantium veritatis in eum dolorum error dolore neque autem ut consectetur mollitia libero nam officia.'
+      },
+      {
+        row: 'row row-1',
+        color: 'c-3',
+        icon: 'fa fa-search',
+        title: 'Search & vetting potential matches',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil deleniti, voluptatibus dolores fugiat temporibus quia, laudantium veritatis in eum dolorum error dolore neque autem ut consectetur mollitia libero nam officia.'
+      },
+      {
+        row: 'row row-2',
+        color: 'c-4',
+        icon: 'fa fa-meetup',
+        title: 'Meet your match',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil deleniti, voluptatibus dolores fugiat temporibus quia, laudantium veritatis in eum dolorum error dolore neque autem ut consectetur mollitia libero nam officia.'
+      },
+      {
+        row: 'row row-1',
+        color: 'c-5',
+        icon: 'fa fa-comment',
+        title: 'after date feedback',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil deleniti, voluptatibus dolores fugiat temporibus quia, laudantium veritatis in eum dolorum error dolore neque autem ut consectetur mollitia libero nam officia.'
+      }
+    ];
+   
   return (
     <div>
         <div class="o-h1">
@@ -17,52 +61,18 @@ function Process() {
         <div class="center-line">
             {/* <a href="#" class="scroll-down"><i class="fa fa-claret-up"></i></a> */}
         </div>
-
-        <div class="row row-1" data-aos="flip-left" data-aos-duaration="1000" data-aos-easing="ease-out-cubic">
-          <section class="c-1">
-              <i class="icon fa fa-clipboard-question"></i>
+       
+       {timeline.map(timeline1 =>
+        <div class={timeline1.row} data-aos="flip-left" data-aos-duaration="1000" data-aos-easing="ease-out-cubic">
+          <section class={timeline1.color}>
+              <i class={`icon ${timeline1.icon}`}></i>
               <div class="details">
-                  <span class="title"><span style={{fontSize:"42px", paddingRight: "15px"}}><i class="fa fa-clipboard-question"></i></span>In depth interview</span>
+                  <span class="title"><span style={Span}><i class={timeline1.icon}></i></span>{timeline1.title}</span>
               </div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, iure voluptate cupiditate magni neque modi dolorem quis alias culpa eligendi ratione quidem, officia cumque voluptatem quod recusandae. Quod, molestias doloribus!</p>
+              <p>{timeline1.text}</p>
           </section>
         </div>
-        <div class="row row-2"  data-aos="flip-left" data-aos-duaration="1000" data-aos-easing="ease-out-cubic">
-          <section class="c-2">
-              <i class="icon fa fa-chalkboard"></i>
-              <div class="details">
-                  <span class="title"><span style={{fontSize: "42px", paddingRight: "15px"}}><i class="fa fa-chalkboard"></i></span>date & relationship coaching</span>
-              </div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed doloribus quisquam alias, doloremque maiores placeat, odit, quia inventore magnam consequuntur modi! Animi, quam nesciunt nobis perspiciatis deleniti ullam molestias placeat!</p>
-          </section>
-        </div>
-        <div class="row row-1" data-aos="flip-left" data-aos-duaration="1000" data-aos-easing="ease-out-cubic">
-          <section class="c-3">
-              <i class="icon fa fa-search"></i>
-              <div class="details">
-                  <span class="title"><span style={{fontSize: "42px", paddingRight: "15px"}}><i class="fa fa-search"></i></span>Search & vetting potential matches</span>
-              </div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempore quia consequatur, assumenda non explicabo expedita! Voluptatem maiores magnam laboriosam eum! Possimus vel obcaecati enim est natus nostrum? Obcaecati, itaque.</p>
-          </section>
-        </div>
-        <div class="row row-2" data-aos="flip-left" data-aos-duaration="1000" data-aos-easing="ease-out-cubic">
-          <section class="c-4">
-              <i class="icon fa fa-meetup"></i>
-              <div class="details">
-                  <span class="title"><span style={{fontSize: "42px", paddingRight: "15px"}}><i class="fa fa-meetup"></i></span> Meet your match</span>
-              </div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur repellat quia minima voluptas magni repellendus, debitis quam fugit deserunt id error beatae eum, nemo illo necessitatibus provident sapiente aspernatur pariatur.</p>
-          </section>
-        </div>
-        <div class="row row-1" data-aos="flip-left" data-aos-duaration="1000" data-aos-easing="ease-out-cubic">
-          <section class="c-5">
-              <i class="icon fa fa-comment"></i>
-              <div class="details">
-                  <span class="title"><span style={{fontSize: "42px", paddingRight: "15px"}}><i class="fa fa-comment"></i></span> after date feedback</span>
-              </div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad corrupti amet in et. Magnam necessitatibus nesciunt, quidem nulla doloremque, obcaecati repellendus commodi nemo minus eum, culpa vitae dignissimos dolorum dolores!</p>
-          </section>
-        </div>
+        )}
     </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'antd/dist/antd.css';
+// import 'antd/dist/antd.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
